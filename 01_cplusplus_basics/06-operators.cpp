dc38ffe2d5ca43dbdcfcdea97d632bbad39cc737
zaferyavuz <yavuzzafer@gmail.com>
2018-09-13 19:27:13 +0300

    bitwise operatör eklendi

--- 01_cplusplus_basics/06-operators.cpp
+++ 01_cplusplus_basics/06-operators.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
  
-main() {
-/*   
+int main() {
+   
    // Aritmetik operatörler
-   int a = 21, b = 10;
-   cout << a+b << endl ;
+   int a1 = 21, b1 = 10;
+   cout << a1+b1 << endl ;
    // + - * / % ++ --
-*/   
+   
 
 /*
   // Relational (İlişkisel) Operatörler (Şart ifadelerinde kullanılır) - sonucu mantıksal dır
@@ -23,6 +23,7 @@ main() {
 
 /*
   // Mantıksal (Logic) Operatörler
+  // &&, ||, !
   int a=1; // bool a=true;
   int b=0; // bool b=false;
   int x=5,y=9;
@@ -31,5 +32,18 @@ main() {
   } 
 */
 
+/*
+  // Bitwise Operator (And Or, xor)
+  // &, |, ^, ~, <<, >>
+    unsigned int a = 60;	  // 60 = 0011 1100  
+    unsigned int b = 13;	  // 13 = 0000 1101
+   
+    cout<< (a & b) <<endl;             // 12 = 0000 1100
+    cout<< (a | b) <<endl;             // 61 = 0011 1101
+    cout<< (a ^ b) <<endl;             // 49 = 0011 0001
+    cout<< signed(~a) <<endl;          // -61 = 1100 0011
+    cout<< (a << 2) <<endl;            // 240 = 1111 0000
+    cout<< (a >> 2) <<endl;            // 15 = 0000 1111
+*/
    return 0;
 }
