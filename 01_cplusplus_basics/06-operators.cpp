53329e69ad414627b00e30342dedb9f4e6b57019
zaferyavuz <yavuzzafer@gmail.com>
2018-09-13 18:53:21 +0300

    aritmetik + ilişkisel + mantıksal

---
+++ 01_cplusplus_basics/06-operators.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+ 
+main() {
+/*   
+   // Aritmetik operatörler
+   int a = 21, b = 10;
+   cout << a+b << endl ;
+   // + - * / % ++ --
+*/   
+
+/*
+  // Relational (İlişkisel) Operatörler (Şart ifadelerinde kullanılır) - sonucu mantıksal dır
+  int a=20,b=10;
+  if(a==b){
+    cout<<"Şart sağlandı";
+  }
+  else{
+    cout<<"Şart sağlanmadı";
+  }
+  // ==, !=, <, >, <=, >=, 
+*/
+
+/*
+  // Mantıksal (Logic) Operatörler
+  int a=1; // bool a=true;
+  int b=0; // bool b=false;
+  int x=5,y=9;
+  if (a && b){ // (x<y || x==3)
+    cout<< a&&b <<endl;
+  } 
+*/
+
+   return 0;
+}
