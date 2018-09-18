7857cc0eb15d5b37007f1ded56a74bbb3cea5d26
zaferyavuz <yavuzzafer@gmail.com>
2018-09-18 14:31:53 +0300

    switch-case eklendi

--- 01_cplusplus_basics/07-karar deyimleri if-else.cpp
+++ 01_cplusplus_basics/07-karar deyimleri if-else.cpp
@@ -19,6 +19,7 @@ int main() {
   else {              // if deyimindeki koşul sağlanmazsa
     cout<<ort<<" -> Geçti\n";
   }
+
 //---------------------------------------------------
 // koşul operatörü kullanımı (sadece eşitleme durumunda)
  /* 
@@ -41,11 +42,78 @@ int main() {
   */
 //--------------------------------------------------------
  /*
- // çoklu şartlarda koşul operatörü
+ // çoklu şartlarda koşul operatörü (nested)
   string sonuc="";
   sonuc=(ort<40)?"Kaldı":(ort<55)?"Şartlı Geçti":"Geçti";
   cout<<ort<<" -> "<<sonuc<<"\n";
 */
+//--------------------------------------------------------
+/*
+  // nested (içiçe) koşul ifadeleri
+  double kanaat = 3.5;
+  if(ort<45){
+    cout<<ort<<" -> Kaldı"<<endl;
+    if (ort>39 && kanaat >2.5){
+      cout<<ort<<" -> Şartlı Geçebilir"<<endl;
+    }
+  }
+*/
+//------------------------------------------------------
+/*
+  kontrol edilecek koşul sadece eşitlik durumunu içeriyorsa switch-case kullanılması daha uygundur.
+*/
+/*
+  char harf=' ';
+  if(ort<40)
+    harf='F';
+  else if(ort<45)
+    harf='D';
+  else if(ort<60)
+    harf='C';
+  else if(ort<80)
+    harf='B';
+  else if(ort<=100)
+    harf='A';
+  else
+    harf='E';
+// neden else if içerisinde aralık kontrol edilmiyor?
+  cout<<ort<<" -> ";
+  switch(harf){
+    case 'A':
+      cout<<"Mükemmel"<<endl;
+      break;
+    case 'B':
+      cout<<"İyi iş çıkardın"<<endl;
+      break;
+    case 'C':
+      cout<<"Dersi geçtin, iyisin"<<endl;
+      break;
+    case 'D':
+      cout<<"Seneye daha ilginç şeyler anlatılacak"<<endl;
+      break;
+    default:
+      cout<<"Eksik Puan"<<endl;
+  }
+  // 1. break kullanılmazsa ne olur?
+  // harf değişkeni string türünden olabilir mi?
+*/
+//----------------------------------------------------
+/*
+  // iç içe swtich-case
+  switch(harf) {
+   case 'A': 
+      cout << "Dış switch-case durumu";
+      switch(ch2) {
+         case 'A':
+            cout << "İç switch-case durumu";
+            break;
+         case 'B': // ...
+      }
+      break;
+   case 'B': // ...
+   // not yazılımı üzerinden bir örnek düşününü ve kodlayın...
+}
+*/  
 
   return 0;
 }
