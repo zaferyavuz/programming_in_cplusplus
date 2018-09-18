c02a6030eb2fe96a6de740fe6efa69c43f4a95e8
zaferyavuz <yavuzzafer@gmail.com>
2018-09-18 17:05:25 +0300

    cin,cout,setw(n),setprecision(n),fixed

--- 01_cplusplus_basics/09-basit girdi çıktı.cpp
+++ 01_cplusplus_basics/09-basit girdi çıktı.cpp
@@ -1,2 +1,53 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+int main() {
+/*
+    <iostream>
+      bu kütüphanede standart giriş çıkış fonksiyonları tanımlıdır
+      cout, cin, cerr, clog
+
+    <iomanip>
+      bu kütüphanede formatlı giriş çıkış fonksiyonları tanımlıdır
+      setw, setprecision
+
+    <fstream>
+      bu kütüphanede dosya üzerinde işlem yapan fonksiyonlar ve türler tanımlıdır,
+      fstream, ifstream, ofstream ...
+*/
+//-------------------------------------------------------
+///*
+  char ad[50];
+  cout << "Adınız : ";
+  cin >> ad;
+  cout << "Merhaba " << ad << endl;
+//*/
+//------------------------------------------------------
+// setw(n) formatlı yazdırma fonksiyonu
+// #include <iomanip> eklenmeli
+// setprecison - hane sayısını belirler
+/*
+  cout<<setw(15)<<"Ad";
+  cout<<setw(15)<<"Arasınav";
+  cout<<setw(15)<<"Final";
+  cout<<setw(15)<<"Ortalama";
+  cout<<endl;
+  int a=25,f=55;            // rasgele başlangıç değerleri
+  double a_d,f_d,ort;
+  for(int i=0;i<10;i++){
+    a=(a*f*f+53)%100;          // 0-100 arası bir not elde et
+    f=(f*a*a+71)%100;
+    a_d = (double)a;
+    f_d = (double)f;
+    ort=0.35165* a_d + 0.64835*f_d;
+    cout<<setw(15)<<i+1;
+    cout<<setw(15)<<a;
+    cout<<setw(15)<<f;
+    cout<<setw(15)<<setprecision(10)<<ort<<endl;
+    //cout<<setw(15)<<fixed<<setprecision(10)<<ort<<endl;
+  }
+*/
+//--------------------------------------------------------
+  return 0;
+}
