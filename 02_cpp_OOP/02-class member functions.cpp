c646a628fea0dee069edae3c5271f8af1b839639
zaferyavuz <yavuzzafer@gmail.com>
2018-09-20 20:50:37 +0300

    Create 02-class member functions.cpp

---
+++ 02_cpp_OOP/02-class member functions.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+class Box {
+   public:
+      double length;         // Length of a box
+      double breadth;        // Breadth of a box
+      double height;         // Height of a box
+
+      // Member functions declaration
+      double getVolume(void);
+      void setLength( double len );
+      void setBreadth( double bre );
+      void setHeight( double hei );
+};
+
+/*
+    :: operatörü (scope resolution operator) ile method (davranış, işlev) tanımlanmaktadır
+
+*/
+
+// Member functions definitions
+double Box::getVolume(void) {
+   return length * breadth * height;
+}
+
+void Box::setLength( double len ) {
+   length = len;
+}
+void Box::setBreadth( double bre ) {
+   breadth = bre;
+}
+void Box::setHeight( double hei ) {
+   height = hei;
+}
+
+// Main function for the program
+int main() {
+   Box Box1;                // Declare Box1 of type Box
+   Box Box2;                // Declare Box2 of type Box
+   double volume = 0.0;     // Store the volume of a box here
+ 
+   // box 1 specification
+   Box1.setLength(6.0); 
+   Box1.setBreadth(7.0); 
+   Box1.setHeight(5.0);
+
+   // box 2 specification
+   Box2.setLength(12.0); 
+   Box2.setBreadth(13.0); 
+   Box2.setHeight(10.0);
+
+   // volume of box 1
+   volume = Box1.getVolume();
+   cout << "Volume of Box1 : " << volume <<endl;
+
+   // volume of box 2
+   volume = Box2.getVolume();
+   cout << "Volume of Box2 : " << volume <<endl;
+   return 0;
+}
