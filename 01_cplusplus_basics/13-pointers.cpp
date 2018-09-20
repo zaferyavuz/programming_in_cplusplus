51362669cad073bac2175af75b2fb64ff9b41514
zaferyavuz <yavuzzafer@gmail.com>
2018-09-20 17:59:09 +0300

    basit pointer mantığı

--- 01_cplusplus_basics/13-pointers.cpp
+++ 01_cplusplus_basics/13-pointers.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+int main () {
+   int  var = 93;   
+   int  *i;        
+
+   i = &var;       
+
+   cout << "var içeriği: ";
+   cout << var << endl;
+
+   cout << "Pointer değeri: ";
+   cout << i << endl;
+
+   cout << "Pointer içeriği: ";
+   cout << *i << endl;
+
+   return 0;
+}
