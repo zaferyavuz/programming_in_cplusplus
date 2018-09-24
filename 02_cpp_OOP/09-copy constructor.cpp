938de23aced45890bcec7ca24ca2eea7f111eeb6
zaferyavuz <yavuzzafer@gmail.com>
2018-09-24 13:13:55 +0300

    örnek eklendi

--- 02_cpp_OOP/09-copy constructor.cpp
+++ 02_cpp_OOP/09-copy constructor.cpp
@@ -1,1 +1,49 @@
-#
+#include <iostream>
+using namespace std;
+/*
+    Copy Constructor - Kopya Yapıcı
+      Nesne kopayalama 2 şekilde yapılır. 
+        1) shallow copy: sadece adresler kopyalanır, 
+        2) deep copy: buradaiçerik değişkenler de kopyalanır. böylece adreste 2 farklı nesne olmuş olur.
+
+      . Kopyalama işleminin nasıl tanımlanacağı kullanıcı tarafından tanımlanırken kullanılır.  
+*/
+//-------------------------------------------------------------
+class Point 
+{ 
+  private: 
+    int x, y; 
+  public: 
+      Point(int x1, int y1) { 
+        x = x1; 
+        y = y1; 
+      } 
+    
+      // Copy constructor 
+      Point(const Point &p2) {
+        x = p2.x; // kopyalarken değişiklik yapılabilir
+        y = p2.y; 
+      } 
+    
+      int getX(){  
+        return x; 
+      } 
+      int getY(){  
+        return y; 
+      } 
+}; 
+
+int main() {
+  
+    Point p1(10, 15); // Normal constructor
+    Point p2 = p1; // Copy constructor çağrılıyor
+  
+     
+    cout << "p1.x = " << p1.getX() << ", p1.y = " << p1.getY()<<endl; 
+    cout << "p2.x = " << p2.getX() << ", p2.y = " << p2.getY()<<endl; 
+
+    cout<<&p1<<endl;
+    cout<<&p2;
+
+  return 0;
+}
