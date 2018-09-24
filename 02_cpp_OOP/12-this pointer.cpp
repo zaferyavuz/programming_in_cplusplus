2468b069ecf19af962c0ad388e205689275ac185
zaferyavuz <yavuzzafer@gmail.com>
2018-09-24 16:00:24 +0300

    coded

--- 02_cpp_OOP/12-this pointer.cpp
+++ 02_cpp_OOP/12-this pointer.cpp
@@ -1,1 +1,48 @@
-#
+#include <iostream>
+ 
+using namespace std;
+
+/*
+    this işaretçisi,
+      . her sınıfın dahili olarak kullanılan bir işaretçisi vardır (this)
+      . pointer olduğu için -> ile kullanılır
+      . genellikle parametre olarak sınıfın kendisini alan fonksiyonlarda karışıklığı önlemek için kullanılır
+      . friend fonk. this ile kullanılmaz (çünkü zaten üye değiller)
+      . ayrıca static fonk. larda this işaretçisi kullanılmaz (gelecek derslerde anlatılacak)
+
+*/
+
+class Box {
+   public:
+      // Constructor definition
+      Box(double l = 2.0, double b = 2.0, double h = 2.0) {
+         cout <<"Constructor called." << endl;
+         length = l;
+         breadth = b;
+         height = h;
+      }
+      double Volume() {
+         return length * breadth * height;
+      }
+      int compare(Box box) {
+         return this->Volume() > box.Volume();
+      }
+      
+   private:
+      double length;     // Length of a box
+      double breadth;    // Breadth of a box
+      double height;     // Height of a box
+};
+
+int main(void) {
+   Box Box1(3.3, 1.2, 1.5);    // Declare box1
+   Box Box2(8.5, 6.0, 2.0);    // Declare box2
+
+   if(Box1.compare(Box2)) {
+      cout << "Box2 is smaller than Box1" <<endl;
+   } else {
+      cout << "Box2 is equal to or larger than Box1" <<endl;
+   }
+   
+   return 0;
+}
