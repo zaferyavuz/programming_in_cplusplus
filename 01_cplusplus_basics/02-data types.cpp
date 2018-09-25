44050469ae4ff1d9500c96c9124adf7a50ec9ebc
zaferyavuz <yavuzzafer@gmail.com>
2018-09-25 18:17:22 +0300

    bitset

--- 01_cplusplus_basics/02-data types.cpp
+++ 01_cplusplus_basics/02-data types.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
 int main() {
 
-   bool dolu = false;    // true(1), false(0) 
+   bool dolu = true;    // true(1), false(0) 
    cout << "Doluluk Durumu        : " << dolu << endl;
    cout << "Boolean bellek boyutu : " << sizeof(bool) << endl;
+   cout << "İkili Gösterim: "<<bitset<8>(dolu)<<endl;
 
 
 /*
    char ch='A'; // ch='b', ch=70
    cout << "Karakter Icerigi      : " << ch << "(" << int(ch) << ")" << endl;
    cout << "char bellek boyutu    : " << sizeof(char) << endl;
+  cout << "İkili Gösterim: "<<bitset<8>(ch)<<endl;
 */
-
 /*
    int n=-500;
    cout << "Tam Sayı İçeriği      : " << n << endl;
@@ -25,24 +27,22 @@ int main() {
 /*
    short int i;           // signed, unsigned farkı
    short unsigned int j;  
-
    j = 50000;
-
    i = j;  // i=65536-50000
    cout << i << " " << j;
 */   
-   
-/*
-   float x=2.5;         // double x=-1.0;
+ /*  
+
+   float x=1.11;         // double x=-1.0;
    cout << "Reel Sayı İçeriği    : " << x << endl;
    cout << "float bellek boyutu  : " << sizeof(float) << endl;
    cout << "double bellek boyutu : " << sizeof(double) << endl;
+   cout << "İkili Gösterim: "<<bitset<32>(x)<<endl;
 */
    
 /*
    // - bazı değişkenler sadece belli değerleri alabilir             
    // - enum anahtar kelimesi ile yeni bir tip tanımlanabilir
-
    //enum color {red,green,blue}; // ilk değer 0 olarak alınır. red=0, green=1, blue=2
    enum color {red, green=5,blue};
    color c = red;     
@@ -66,19 +66,15 @@ int main() {
         float           float    
         double          double
         void            değer içermeyen
-
         *********** Veri Tipi Düzenleyici Anahtar Kelimeleri *******************
         signed                  hiç bir şey yazılmazsa "signed" olarak alınır
         unsigned
         short
         long
-
         ***********************************************************
-
         char	                1byte	        -127 to 127 
         unsigned char	        1byte	        0 to 255
         signed char	        1byte	        -127 to 127
-
         int	                4bytes	        -2147483648 to 2147483647
         unsigned int	        4bytes	        0 to 4294967295
         signed int	        4bytes	        -2147483648 to 2147483647
@@ -92,14 +88,12 @@ int main() {
         float	                4bytes	        +/- 3.4e +/- 38 (~7 digits)
         double	                8bytes	        +/- 1.7e +/- 308 (~15 digits)
         long double	        8bytes	        +/- 1.7e +/- 308 (~15 digits) (bazı derleyicilerde 16)
-
         *********************************************************************
         typedef Tanımlama
                 - c++ da typedef kullanarak var olan t,iplerden yeni bir tip tanımlanabilir
                 - örneğin;
                         typedef float hacim;
                         hacim hacim1;           // hacim anahtar kelimesi artık float olarak değerlendirilecek
-
         *********************************************************************
         enum
                 - değişkenler billi bilgileri tutmak için kullanılır ve belirli tiplerde tanımlanır
