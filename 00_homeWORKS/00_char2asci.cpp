ee41fd70bcd208e84abb204b057e7494c9773827
zaferyavuz <yavuzzafer@gmail.com>
2018-09-28 09:56:21 +0300

    ödev oluşturuldu.

---
+++ 00_homeWORKS/00_char2asci.cpp
@@ -0,0 +1,9 @@
+/*  KARAKTERLER VE ASCI KARŞILIKLARI
+- tüm karakterlerin ascii karşılıklarını yazınız
+- özel karaterlerin asci karşılıklarını yazınız
+- kalvyeden girilen karakterin asci karşılığını yazınız
+
+Özel Ödev:
+  türkçe karakterlerin asci karşılıkları hakkında araştırma yapınız, 
+  buna göre girilen türkçe bir karakterin asci karşılığını ekrana yazınız.
+----------------------------------------------------------*/
