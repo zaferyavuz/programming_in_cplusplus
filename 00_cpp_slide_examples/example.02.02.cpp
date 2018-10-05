6d7a67f2193321dc46281d8888382e895a170d40
zaferyavuz <yavuzzafer@gmail.com>
2018-10-05 13:27:01 +0300

    Update example.02.02.cpp

--- 00_cpp_slide_examples/example.02.02.cpp
+++ 00_cpp_slide_examples/example.02.02.cpp
@@ -1,7 +1,17 @@
 /*
-The following are legal identifiers in C++.
-first
-conversion
-payRate
-counter1
+DEĞİŞKEN İSİMLENDİRME KURALLARI
+Aşağıdaki ifadeler C++ dilinde doğru değişken ifadleridir.
+    first
+    conversion
+    payRate
+    counter1
+    first_num
+    _note
+
+hatalı değişken isimlendirmeleri
+    1num              first digit is not allowed as numeric
+    num#?             special chars not allowed except underscore _
+    first-num         - not allowed
+    for               reserved words
+    true
 */
