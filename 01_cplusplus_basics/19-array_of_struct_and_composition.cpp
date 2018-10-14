8fda67b4e6593da8e2a6e9464462f3000fe6b442
zaferyavuz <yavuzzafer@gmail.com>
2018-10-14 14:11:12 +0300

    Update 19-array_of_struct_and_composition.cpp

--- 01_cplusplus_basics/19-array_of_struct_and_composition.cpp
+++ 01_cplusplus_basics/19-array_of_struct_and_composition.cpp
@@ -1,1 +1,19 @@
-#
+#include <iostream>
+using namespace std;
+
+int main() {
+  //..............
+/*
+  örnek uygulama:
+  isim - ad ve soyaddan oluşur
+  telefon - ulke kodu, sehir kodu ve telefon numarasından oluşur
+  sınav_sonucu: ders kodu, arasınav, dönem sonu, bütünleme ve harfli nottan oluşur
+  zaman: yıl, ay, gün, saat, dakika, saniye ve salise den oluşur
+  
+  öğrencinin: isim, öğrenci numarası, kayıt zamanı (uzun zaman) telefon, eposta, 50 dersten aldığı notlardan oluşur.
+  
+  10 öğrencinin bilgilerini dosyadan okuyalım, harfli notlarını hesaplayalım ve dosyaya yazalım
+
+*/
+  return 0;
+}
