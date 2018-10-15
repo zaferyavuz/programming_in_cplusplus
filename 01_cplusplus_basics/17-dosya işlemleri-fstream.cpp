2f77bf7f3020f4ed96dd3597e00766c190ce3f8e
zaferyavuz <yavuzzafer@gmail.com>
2018-10-15 13:04:17 +0300

    Update 17-dosya işlemleri-fstream.cpp

--- 01_cplusplus_basics/17-dosya işlemleri-fstream.cpp
+++ 01_cplusplus_basics/17-dosya işlemleri-fstream.cpp
@@ -3,47 +3,15 @@
 #include <iostream>
 using namespace std;
 /*
-1	
-ofstream
+1  ofstream    This data type represents the output file stream and is used to create files and to write information to files.
+2  ifstream    This data type represents the input file stream and is used to read information from files.
+3	fstream     This data type represents the file stream generally, and has the capabilities of both ofstream and ifstream which means it can create files, write information to files, and read information from files.
 
-This data type represents the output file stream and is used to create files and to write information to files.
-
-2	
-ifstream
-
-This data type represents the input file stream and is used to read information from files.
-
-3	
-fstream
-
-This data type represents the file stream generally, and has the capabilities of both ofstream and ifstream which means it can create files, write information to files, and read information from files.
-
-
-1	
-ios::app
-
-Append mode. All output to that file to be appended to the end.
-
-2	
-ios::ate
-
-Open a file for output and move the read/write control to the end of the file.
-
-3	
-ios::in
-
-Open a file for reading.
-
-4	
-ios::out
-
-Open a file for writing.
-
-5	
-ios::trunc
-
-If the file already exists, its contents will be truncated before opening the file.
-
+1  ios::app    Append mode. All output to that file to be appended to the end.
+2	ios::ate    Open a file for output and move the read/write control to the end of the file.
+3	ios::in     Open a file for reading.
+4	ios::out    Open a file for writing.
+5	ios::trunc  If the file already exists, its contents will be truncated before opening the file.
 */
 
 
