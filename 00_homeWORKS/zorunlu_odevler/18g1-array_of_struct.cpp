f3bc73e59bed5073699d61d8b6ece7ae4724193b
zaferyavuz <yavuzzafer@gmail.com>
2018-10-15 09:09:40 +0300

    Update 18g1-array_of_struct.cpp

--- 00_homeWORKS/zorunlu_odevler/18g1-array_of_struct.cpp
+++ 00_homeWORKS/zorunlu_odevler/18g1-array_of_struct.cpp
@@ -1,1 +1,32 @@
 #
+
+
+
+/*
+Bir PC ye bağlı yazıcıda çıktı alırken PC den gönderilen belgeler yazıcı kuyruğunda birikmektedir. PC belgeleri bu kuyruğa iter, 
+yazıcı ise işi bittiğinde bu kuyruktan sırasıyla bir belge alır ve yazdırır. 
+Sizden bu sürecin “dairesel kuyruk” veri yapısı ile kodlanması istenmektedir. Aşağıda kısıtlar ve istenenler listelenmektedir.
+Yazıcı kuyruğunda en fazla 20 belge biriktirilebilmektedir ve bu değer program test edilirken değiştirilebilir (kapasite=20). 
+PC herhangi bir zamanda kuyruğa bir belge ekleyebilir. Yazıcı herhangi bir zamanda kuyruktan bir belge alıp yazdırabilir.
+Kuyruk dolu olduğunda PC kuyruğa belge ekleyemez ve hata mesajı alır, Kuyruk boş olduğunda yazıcı bir şey yazmaz.
+Belgeler PC den gönderilme sıralarına göre yazdırılmalıdır.
+Örnek bir main dosyası ayrıca yayınlanacaktır ve main içindeki kodlara bağlı kalınacaktır.
+Ödev savunması sırasında kaynak kod üzerinde öğrenci tarafından istenen değişiklikler yapılabilecektir.
+Yazıcıya gönderilen belgelerin özellikleri şu şekilde olmalıdır
+Belge adı/yolu
+boyutu
+belgenin sahibi (kisi türünde bir yapı)
+  ad
+  soyad
+  kullanıcı adı
+  eposta
+  doğum yılı
+oluşturulma tarihi (tarih türünden bir yapı)
+  yıl
+  gün
+  ay
+  saat
+  dakika
+  saniye
+
+*/
