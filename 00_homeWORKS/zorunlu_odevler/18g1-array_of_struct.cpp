673e89745320bd752eb28b63a629c099c9af23aa
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2018-10-18 22:19:25 +0300

    Ornek senaryo eklendi

--- 00_homeWORKS/zorunlu_odevler/18g1-array_of_struct.cpp
+++ 00_homeWORKS/zorunlu_odevler/18g1-array_of_struct.cpp
@@ -32,8 +32,44 @@ int main() {
 /*
 Örnek Dosya İçeriği
 --------------------------------
-files/a.docx 2403345 4 zafer;yavuz;zyavuz;zyavuz@ktu.edu.tr;1985 2018:10:21:14:05:56
-files/a.docx 2403345 4 zafer;yavuz;zyavuz;zyavuz@ktu.edu.tr;1985 2018:10:21:14:05:56
+files/a.docx 2403345 4 zafer yavuz zyavuz zyavuz@ktu.edu.tr 1985 2018 10 21 14 05 56
 ....
 ....
+....
+bu şekilde belge sayısı kadar satir olacak.
+
+sozgelimi dosyada 10 adet belge olsun
+kuyruk kapasitesi 5 olsun.
+pc kuyruga 2 saniyede bir gondersin.
+yazici ise belgeyi 5sn de bir yazsin.
+
+bu durumda ornek dosya girisi
+a1 
+a2
+a3
+a4
+...
+...
+...
+a10
+
+olsun
+
+hededlenen ekran çıktısı 
+2.sn pc a1 gonderdi. kuyruk. a1
+4.sn pc a2 gönderdi. kuyruk. a1 a2
+5.sn yazıcı a1 yazdirdi. kuyruk a2
+6.sn pc a3 gonderdi. kuyruk a2 a3
+8.sn pc a4 gonderdi. kuyruk. a2 a3 a4
+10.sn pc a5 gonderdi. kuyruk. a2 a3 a4 a5
+10.sn yazici a2 yazdirdi. kuyruk. a3 a4 a5
+.....
+16.sn pc a8 gonderdi. kuyruk. a4 a5 a6 a7 a8
+18.sn pc a9 gonderdi. hata. kuyruk dolu bekle
+20.sn pc a9 gonderdi. hata. kuyruk dolu bekle 
+20.sn yazici a4 yazdirdi. kuyruk. a5 a6 a7 a8
+22.sn pc a9 gonderdi. kuyruk. a5 a6 a7 a8 a9
+.....
+50.sn yazici a10 yazdirdi. kuyruk. boş 
+pc gonderim listesi bos. kuyruk bos. sonlandı. 
 */
