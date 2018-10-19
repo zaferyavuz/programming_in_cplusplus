f6178f9c3a09dc8da4884529cfa2609f121d5f85
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2018-10-19 19:02:57 +0300

    çıktı dosya eklendi

--- 00_homeWORKS/10_array_of_struct.cpp
+++ 00_homeWORKS/10_array_of_struct.cpp
@@ -62,6 +62,18 @@ Ferdi Besli 124 45 60 0 E
 Zeynep Yılmaz 125 50 100 0 E
 Merve Yavuz 126 50 60 0 E
 Ali Yağız 127 50 60 0 E
+
+
+Çıktı Dosya
+--------------------------------
+5
+Zafer Yavuz 50 60 55 CB
+Ferdi Besli 45 60 52.5 CC
+Zeynep Yılmaz 50 100 75 BA
+Merve Yavuz 50 60 55 CB
+Ali Yağız 50 60 55 CB
+
+
 */
 
 int main () {
@@ -96,22 +108,3 @@ int main () {
   }
    return 0;
 }
-
-
-/*
-a.txt
-------------------
-abadi
-abajur
-abak
-abakan
-abala
-abandone
-abanges
-abani
-abanma
-abanmak
-abanoz
-abanozlaţmak
-abar
-*/
