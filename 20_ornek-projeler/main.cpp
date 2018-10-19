e38445bed00cfd11c52b9f29ff964fa36a4d7ace
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2018-10-19 19:05:43 +0300

    Update main.cpp

--- 20_ornek-projeler/main.cpp
+++ 20_ornek-projeler/main.cpp
@@ -1,1 +1,7 @@
-
+/*
+  Uygulma amacı, programın ne iş yaptığı
+  Ad Soyad
+  iletişim bilgileri, eposta, web sitesi vs...
+  son güncelleme tarihi
+  opsiyonel extra bilgiler
+------------------------------------*/
