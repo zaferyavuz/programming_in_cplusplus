798998f5fb317b374e7a2761b5eecc81ee5b74f2
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2018-10-21 11:22:32 +0300

    Update 11_array_of_struct_and_file_processing.cpp

--- 00_homeWORKS/11_array_of_struct_and_file_processing.cpp
+++ 00_homeWORKS/11_array_of_struct_and_file_processing.cpp
@@ -18,7 +18,6 @@ dosyadan okuma olacak
 /*
 Dosya içeriği örneği
 ---------------------------------
-5
 Zafer Yavuz 123 50 60 0 E
 Ferdi Besli 124 45 60 0 E
 Zeynep Yılmaz 125 50 100 0 E
@@ -26,18 +25,18 @@ Merve Yavuz 126 50 60 0 E
 Ali Yağız 127 50 60 0 E
 Çıktı Dosya
 --------------------------------
-5
-Zafer Yavuz 50 60 55 CB
-Ferdi Besli 45 60 52.5 CC
-Zeynep Yılmaz 50 100 75 BA
-Merve Yavuz 50 60 55 CB
-Ali Yağız 50 60 55 CB
+Zafer Yavuz 123 50 60 56.5 CB
+Ferdi Besli 124 45 60 54.75 CC
+Zeynep Yılmaz 125 50 100 82.5 BA
+Merve Yavuz 126 50 60 56.5 CB
+Ali Yağız 127 50 60 56.5 CB
 */
 
 #include <fstream>
 #include <iostream>
-#define AK 0.5
-#define FK 0.5
+#define AK 0.35
+#define FK 0.65
+#define N 100
 using namespace std;
 
 struct TamAd{
@@ -66,13 +65,15 @@ string HarfNotHesapla(double o){
   else return "E";
 }
 int main () {
-  string dosyaAd = "a.txt";
+  string dosyain = "a.txt";
+  string dosyaout = "a.txt";
+
+  cout<<"dosyadan okuma başladı"<<endl;
   ifstream dosya;
-  dosya.open(dosyaAd.c_str());
-  int N;
-  dosya>>N;
+  dosya.open(dosyain.c_str());
   Ogrenci s1[N];
-  for(int i=0;i<N;i++){
+  int i=0;
+  while(!dosya.eof()){
     dosya>>s1[i].tamAd.ad;
     dosya>>s1[i].tamAd.soyad;
     dosya>>s1[i].numara;
@@ -82,18 +83,26 @@ int main () {
     dosya>>s1[i].harfNot;
     s1[i].ortalama = s1[i].ortalama = OrtalamaHesapla(s1[i].araSNot, s1[i].finalNot);
     s1[i].harfNot = HarfNotHesapla(s1[i].ortalama);
+    i++;
   }
   dosya.close();
+  cout<<"dosyadan okuma bitti"<<endl;
+
+
+
+  cout<<"dosyadan yazma başladı"<<endl;
   ofstream dosya2;
-  dosya2.open(dosyaAd.c_str());
-  dosya2<<N<<endl;
-  for(int i=0;i<N;i++){
-    dosya2<<s1[i].tamAd.ad<<" ";
-    dosya2<<s1[i].tamAd.soyad<<" ";
-    dosya2<<s1[i].araSNot<<" ";
-    dosya2<<s1[i].finalNot<<" ";
-    dosya2<<s1[i].ortalama<<" ";
-    dosya2<<s1[i].harfNot<<endl;
+  dosya2.open(dosyaout.c_str());
+  for(int j=0;j<i;j++){
+    dosya2<<s1[j].tamAd.ad<<" ";
+    dosya2<<s1[j].tamAd.soyad<<" ";
+    dosya2<<s1[j].numara<<" ";
+    dosya2<<s1[j].araSNot<<" ";
+    dosya2<<s1[j].finalNot<<" ";
+    dosya2<<s1[j].ortalama<<" ";
+    dosya2<<s1[j].harfNot;
+    if(j!=i-1) dosya2<<endl;
   }
+  cout<<"dosyadan yazma bitti"<<endl;
    return 0;
 }
