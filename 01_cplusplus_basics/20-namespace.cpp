61a683c3cd9261a95498a9466914bbe3f7256712
zaferyavuz <yavuzzafer@gmail.com>
2018-10-22 13:51:31 +0300

    Create 20-namespace.cpp

---
+++ 01_cplusplus_basics/20-namespace.cpp
@@ -0,0 +1,110 @@
+/*
+namespace anahtar kelimesi ile tanımlanır.
+
+namespace namespace_name {
+   // code declarations
+}
+
+--------------------------------------------------------
+:: operatörü ile alt elemanlara erişilebilir
+
+name::code;  // code could be variable or function.
+
+--------------------------------------------------------
+örnek:
+
+#include <iostream>
+using namespace std;
+
+// first name space
+namespace first_space {
+   void func() {
+      cout << "Inside first_space" << endl;
+   }
+}
+
+// second name space
+namespace second_space {
+   void func() {
+      cout << "Inside second_space" << endl;
+   }
+}
+
+int main () {
+   // Calls function from first name space.
+   first_space::func();
+   
+   // Calls function from second name space.
+   second_space::func(); 
+
+   return 0;
+}
+
+çıktı:
+Inside first_space
+Inside second_space
+---------------------------------------------------------
+using namespace ile seçeceğimiz ad alanıında çalışabiliriz.
+#include <iostream>
+using namespace std;
+
+// first name space
+namespace first_space {
+   void func() {
+      cout << "Inside first_space" << endl;
+   }
+}
+
+// second name space
+namespace second_space {
+   void func() {
+      cout << "Inside second_space" << endl;
+   }
+}
+
+using namespace first_space;
+int main () {
+   // This calls function from first name space.
+   func();
+   
+   return 0;
+}
+
+-------------------------------------------------------------------------
+sadece obje adıni bile kullanabiliriz.
+
+#include <iostream>
+using std::cout;
+
+int main () {
+   cout << "std::endl is used with std!" << std::endl;
+   
+   return 0;
+}
+------------------------------------------------------------------------
+nested namespaces
+
+#include <iostream>
+using namespace std;
+
+// first name space
+namespace first_space {
+   void func() {
+      cout << "Inside first_space" << endl;
+   }
+   
+   // second name space
+   namespace second_space {
+      void func() {
+         cout << "Inside second_space" << endl;
+      }
+   }
+}
+
+using namespace first_space::second_space;
+int main () {
+   // This calls function from second name space.
+   func();
+   
+   return 0;
+}
