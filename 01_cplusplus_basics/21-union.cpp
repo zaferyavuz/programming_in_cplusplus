9fea0d1d56398ecdaa31371831902b304b6d573c
zaferyavuz <yavuzzafer@gmail.com>
2018-10-22 14:12:34 +0300

    Update 21-union.cpp

--- 01_cplusplus_basics/21-union.cpp
+++ 01_cplusplus_basics/21-union.cpp
@@ -33,3 +33,23 @@ s.n = 12345678
 s.c is now 0
 s.n is now 115678
 */
+====================================================================
+    /*
+The purpose of union is to save memory by using the same memory region for 
+    storing different objects at different times. That's it.
+
+It is like a room in a hotel. Different people live in it for non-overlapping periods of time. 
+    These people never meet, and generally don't know anything about each other. 
+    By properly managing the time-sharing of the rooms 
+    (i.e. by making sure different people don't get assigned to one room at the same time), 
+     a relatively small hotel can provide accomodations to a relatively large number of people, 
+     which is what hotels are for.
+
+That's exactly what union does. If you know that several objects in your program hold values 
+with non-overlapping value-lifetimes, then you can "merge" these objects into a union and thus save memory. 
+Just like a hotel room has at most one "active" tenant at each moment of time, 
+a union has at most one "active" member at each moment of program time. 
+Only the "active" member can be read. 
+By writing into other member you switch the "active" status to that other member.
+
+*/
