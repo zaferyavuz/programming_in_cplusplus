5f14e9952642ff7de4e842cd527d38303b4dfd4e
zaferyavuz <yavuzzafer@gmail.com>
2018-10-23 16:50:38 +0300

    Update 18g1-array_of_struct.cpp

--- 00_homeWORKS/zorunlu_odevler/18g1-array_of_struct.cpp
+++ 00_homeWORKS/zorunlu_odevler/18g1-array_of_struct.cpp
@@ -31,12 +31,30 @@ int main() {
 }
 
 /*
-Örnek Dosya İçeriği
---------------------------------
-files/a.docx 2403345 4 zafer yavuz zyavuz zyavuz@ktu.edu.tr 1985 2018 10 21 14 05 56
-....
-....
-....
+----------------------------------------------------------------------
+ÖRNEK BİR DOSYA 
+files/odev_son.docx	44087	4	zafer	yavuz	zyavuz	zyavuz@ceng.ktu.edu.tr	2014	1	30	1	48	34
+files/odev1.docx	46543	2	sedat	görmüş	sgormus	sedatgormus@ceng.ktu.edu.tr	2013	10	12	2	7	10
+files/rapor.docx	121302	5	ali	demir	ademir	ademir2@hotmail.com	2016	4	15	15	8	6
+files/son_rapor.docx	73762	4	esra	demirel	edemir	edemir@zoho.com	2017	11	13	19	15	8
+files/belge.docx	123394	1	ferdi	besli	fbesli	ferdi@hotmail.com	2013	5	7	9	9	23
+files/gecmis.docx	73468	2	zafer	yavuz	zyavuz	zyavuz@ceng.ktu.edu.tr	2016	11	21	23	3	7
+files/cizelge.pdf	98889	1	ali	demir	ademir	ademir2@hotmail.com	2015	10	22	11	25	42
+files/haftalık-program.pdf	81120	1	esra	demirel	edemir	edemir@zoho.com	2018	10	3	23	43	54
+files/sınav-listesi.pdf	25954	2	zeynep	öztürk	zozturk	zeynep@outlook.com	2015	11	1	7	17	23
+files/odev1.cpp	76016	6	sedat	görmüş	sgormus	sedatgormus@ceng.ktu.edu.tr	2016	11	19	13	59	17
+files/main.cpp	52982	5	sedat	görmüş	sgormus	sedatgormus@ceng.ktu.edu.tr	2015	8	12	12	20	22
+files/kaynak-kod.c	59369	2	sedat	görmüş	sgormus	sedatgormus@ceng.ktu.edu.tr	2017	3	6	5	41	8
+files/izinler.pdf	54018	2	zafer	yavuz	zyavuz	zyavuz@ceng.ktu.edu.tr	2017	7	2	2	42	19
+files/personel-list.docx	8803	2	ferdi	besli	fbesli	ferdi@hotmail.com	2018	11	13	4	33	24
+files/tasarım-raporu.rtf	108384	3	zeynep	öztürk	zozturk	zeynep@outlook.com	2015	9	24	20	29	55
+files/bitirme-tez.odf	70000	1	esra	demirel	edemir	edemir@zoho.com	2018	11	26	22	8	53
+------------------------------------------------------------------------------------------------
+KULLANICIDAN ALINACAK DEĞERLER
+1. PC kaç saniyede kuyrupa belge gönderir (sn) : 2
+2. Yazıcının bir sayfayı yazma hızı nedir? (cn): 1
+
+
 bu şekilde belge sayısı kadar satir olacak.
 
 sozgelimi dosyada 10 adet belge olsun
@@ -79,3 +97,5 @@ hededlenen ekran çıktısı
 50.sn yazici a10 yazdirdi. kuyruk. boş 
 pc gonderim listesi bos. kuyruk bos. sonlandı. 
 */
+
+
