79760be1e90ae2e42aa50e860bf955bc925c4c44
zaferyavuz <yavuzzafer@gmail.com>
2018-10-25 18:22:50 +0300

    Update 11-implementation hiding.cpp

--- 02_cpp_OOP/11-implementation hiding.cpp
+++ 02_cpp_OOP/11-implementation hiding.cpp
@@ -9,59 +9,48 @@ In a include statement, user defined header files are enclosed in double quotes 
 
 */
 
-//-----------------------------------------------------------------------
-//clock.h, the specification file for the class clockType
-
-class clockType
-{
-  public:
-      void setTime(int hours, int minutes, int seconds);
-      void getTime(int& hours, int& minutes, int& seconds);
-      void printTime() const;
-      void incrementSeconds();
-      void incrementMinutes();
-      void incrementHours();
-      bool equalTime(const clockType& otherClock) const;
-      clockType(int hours, int minutes, int seconds);
-      clockType();
-  private:
-      int hr;  //store hours
-      int min; //store minutes
-      int sec; //store seconds
-};
-//-------------------------------------------------------------------------
-//clockImp.cpp, the implementation file
-#include <iostream>
-#include "clock.h"
-
-using namespace std;
-/*
-  .
-  .
-  .
-//The definition of the member functions of the clockType //goes here.
-  .
-  .
-  .
-  */
-//-----------------------------------------------------------------------
-//TestClock.cpp. The user program that uses the class //clockType
-
-#include <iostream>
-#include "clock.h"
-using namespace std;
-.
-.
-.
-//Place the definitions of the function main and the other 
-//user-defined functions here
-.
-.
-.
-//-------------------------------------------------------------------------------
 /*
     deklerasyon obj.h dosyasında yapılır
     obj.h -> dosyasını include eden obj.cpp dosyasında implementasyon yapılır
     obj.h -> dosyasını include eden main dosyasında test edilir
         böylece obj.cpp dosyası hiç bir yerde include edilmeden hiding yapılır (gizlenir)
 */
+//-----------------------------------------------------------------------
+// main.cpp
+#include "obj.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  obj o;
+  cout<<o.ortalamaHesapla()<<endl;
+  return 0;
+}
+
+//----------------------------------------------------------------------
+/*
+// obj.h
+class obj {
+  public:
+    obj();
+    double ortalamaHesapla();
+  private:
+    double araSinavNot;
+    double finalNot;
+    double ortalamaNot;
+};
+
+//--------------------------------------------------------
+// obj.cpp
+#include "obj.h"
+
+obj::obj(){
+  araSinavNot = 50;
+  finalNot    = 60;
+}
+double obj::ortalamaHesapla(){
+  ortalamaNot = 0.5*araSinavNot + 0.5*finalNot;
+  return ortalamaNot;
+}
+*/
+
