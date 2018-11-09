357ca75b6f8179c9a41e6b27c87c5368b0106bbe
zaferyavuz <yavuzzafer@gmail.com>
2018-11-09 18:53:26 +0300

    Update 15-pointer to classes.cpp

--- 02_cpp_OOP/15-pointer to classes.cpp
+++ 02_cpp_OOP/15-pointer to classes.cpp
@@ -44,6 +44,15 @@ int main(void) {
 
    // Now try to access a member using member access operator
    cout << "Volume of Box2: " << ptrBox->Volume() << endl;
+	
+// pointer dizi ilişkisi	
+//	Box *bx1ptr = new Box[20];
+//  cout<<bx1ptr->Volume();  // ilk elemana erişiyor, pointer olarak
+//	cout<<bx1ptr[1].Volume(); // 1. elemana erişiyor, o artık bir pointer değil direk nesneyi işaret ediyor
+//  cout<<(bx1ptr+5)->Volume();		// 5 eleman sonrasına erişiliyor	
+	
+//	Box b2 = *(bx1ptr+4); 		// içerik b2 ye kopyalanıyor
+// 	cout<<b2.Volume();
   
    return 0;
 }
