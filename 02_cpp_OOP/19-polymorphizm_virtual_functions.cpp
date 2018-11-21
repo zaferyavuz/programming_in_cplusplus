8d49b533093bba9fb4d62a837f463d7a468a0029
zaferyavuz <yavuzzafer@gmail.com>
2018-11-21 18:19:40 +0300

    Update and rename 19-virtual_functions_polimorfizm.cpp to 19-polymorphizm_virtual_functions.cpp

--- 02_cpp_OOP/19-polymorphizm_virtual_functions.cpp
+++ 02_cpp_OOP/19-polymorphizm_virtual_functions.cpp
@@ -1,4 +1,77 @@
 /*
+PloyMorphizm
+	* çok biçimlilik anlamı vardır
+	* miras ile yakından ilişkilidir
+	* şu şekilde bir miras sınıf hiyerarşisi olsun
+		- base-class
+			- derived-class1
+			- derived-class2
+	* base-class türünden bir pointer, derived-class1 ve derived-class2 sınıflarını adresleyebilir
+	* böylece aynı kod farklı fonksiyonların çalıştırılmasını sağlayabilir
+	* binding işlemi run-time binding olur, hangi fonksiyonun çağrılacağı koşma zamanında belli olur
+*/
+
+/*
+#include <iostream> 
+using namespace std;
+ 
+class Shape {
+   protected:
+      int width, height;
+      
+   public:
+      Shape( int a = 0, int b = 0){
+         width = a;
+         height = b;
+      }
+      int area() {
+         cout << "Parent class area :" <<endl;
+         return 0;
+      }
+};
+class Rectangle: public Shape {
+   public:
+      Rectangle( int a = 0, int b = 0):Shape(a, b) { }
+      
+      int area () { 
+         cout << "Rectangle class area :" <<endl;
+         return (width * height); 
+      }
+};
+
+class Triangle: public Shape {
+   public:
+      Triangle( int a = 0, int b = 0):Shape(a, b) { }
+      
+      int area () { 
+         cout << "Triangle class area :" <<endl;
+         return (width * height / 2); 
+      }
+};
+
+// Main function for the program
+int main() {
+   Shape *shape;
+   Rectangle rec(10,7);
+   Triangle  tri(10,5);
+
+   // store the address of Rectangle
+   shape = &rec;
+   
+   // call rectangle area.
+   shape->area();
+
+   // store the address of Triangle
+   shape = &tri;
+   
+   // call triangle area.
+   shape->area();
+   
+   return 0;
+}
+*/
+//************************************************************************************************
+/*
 	Virtual Functions (Sanal Fonksiyonlar)
 		* sanal fonksiyonlar görünüşte var olup gerçekte olmayan fonksiyonlardır.
 		* Gerçekte bir gözdesi olmayan fonksiyonlardır
