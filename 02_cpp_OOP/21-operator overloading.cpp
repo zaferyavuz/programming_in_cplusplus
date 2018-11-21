b672b7d26708dc6f80835c92fa90d189486756c3
zaferyavuz <yavuzzafer@gmail.com>
2018-11-21 19:01:07 +0300

    Update 21-operator overloading.cpp

--- 02_cpp_OOP/21-operator overloading.cpp
+++ 02_cpp_OOP/21-operator overloading.cpp
@@ -1,3 +1,27 @@
+/*
+   - saat bilgilerinin toplanması için operator overloading gerekli
+   - benzer şekilde
+      * komplex sayı toplanması
+      * saat karşılaştırma
+      * alan farkı vb..
+   - operator anahtar kelimesi kullanılır
+   - şu operatorler aşırı yüklenemz
+      .   .*   ::   ?:   sizeof
+//------------------------------------------------------------   
+aşırı yükleme, member funk. ya da friend func olarak tanımlanabilir
+operatör AŞIRI YÜKLEME - friend funcion vs. member function
+member func.
+   s1 + s2 ifadesi s1.operator+(s2); haline dönüşür. 
+   Bu şu demek, fonk. ilk objenin alt fonk. olarak çağrılır.
+friend func.
+   ancak s1 + s2 de ilk operand obje değilse problem oluşur.
+   10 + s1, ya da s1 + 10 ifadeleri hata verir
+   s1 + 10 ifadesi aşırı yükleme ile düzeltilebilir
+   ancak yine de private elamanlara erişecek şekilde friend func. tanımlanarak problem çözülür,
+   aşağıda <<, >> kısmında örnek mevcut, bakınız...
+   
+*/
+
 #include <iostream>
 using namespace std;
 
