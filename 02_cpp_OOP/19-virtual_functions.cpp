f10bd7ffed7c7d2a0912ee1df00632fd918280b4
zaferyavuz <yavuzzafer@gmail.com>
2018-11-21 14:12:42 +0300

    Update 19-virtual_functions.cpp

--- 02_cpp_OOP/19-virtual_functions.cpp
+++ 02_cpp_OOP/19-virtual_functions.cpp
@@ -1,3 +1,6 @@
 /*
-
+	Virtual Functions (Sanal Fonksiyonlar)
+		* Gerçekte bir gözdesi olmayan fonksiyonlardır
+		* miras kavramında, base class'te sanal bir fonksiyon tanımlanır ve derived class'ta bu fonksiyon aşırı yüklenir
+		
 */
