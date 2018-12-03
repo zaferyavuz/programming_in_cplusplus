b4b67ee0c48afb10a69d9ca1ce72c84923d6bf10
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2018-12-03 19:58:48 +0300

    Create 18e-dynamic_array_with_classes.cpp

---
+++ 02_cpp_OOP/18e-dynamic_array_with_classes.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+using namespace std;
+
+class Ders{
+  public:
+    Ders(int ogrSayi=4,int odevSayi=3){
+      this->ogrSayi = ogrSayi;
+      this->odevSayi = odevSayi;
+      odevOrtalama = new double[odevSayi];
+      ogrOrtalama  = new double[ogrSayi];
+      notlar = new double*[ogrSayi];
+      for(int i=0;i<ogrSayi;i++){
+        notlar[i] = new double[odevSayi];
+      }
+    }
+    void notlarOku(){
+      for(int i=0;i<ogrSayi;i++){
+        cout<<i+1<<". ogrencinin"<<endl;
+        for(int j=0;j<odevSayi;j++){
+          cout<<j+1<<". ödev notu:";
+          cin>>notlar[i][j];
+        }
+      }  
+    }
+    void ogrOrtalamaBul(){
+      double toplam=0;
+      for(int i=0;i<ogrSayi;i++){
+        toplam=0;
+        for(int j=0;j<odevSayi;j++){
+          toplam += notlar[i][j];
+        }
+        ogrOrtalama[i] = toplam / odevSayi;
+        cout<< i+1 << ". öğrenci ort="
+            << ogrOrtalama[i]<<endl;
+      }
+    }
+    void odevOrtalamaBul(){
+      double toplam=0;
+      for(int j=0;j<odevSayi;j++){
+        toplam = 0;
+        for(int i=0;i<ogrSayi;i++){
+          toplam += notlar[i][j];
+        }
+        odevOrtalama[j] = toplam / ogrSayi;
+        cout<< j+1 << ". ödev ort="
+            << odevOrtalama[j]<<endl;
+      }
+  }
+  private:
+    int ogrSayi;
+    int odevSayi;
+    double **notlar;
+    double *odevOrtalama;
+    double *ogrOrtalama;
+};
+
+
+int main() {
+/*  
+  // 1 boyutlu dinamik bir dizi
+  int *p;
+  int n=10;
+	p = new int[n];
+	p++;
+	*p = 35;
+*/
+//-----------------------------------------
+/*
+// öğrenci not ortalaması
+  int n;
+  cout<<"n:";
+  cin>>n;
+  double toplam=0,max=-9999,min=9999;
+  double *notlar = new double[n];
+  for(int i=0;i<n;i++){
+    cout<<i+1<<" >";
+    cin>>notlar[i];
+    toplam+=notlar[i];
+    if( notlar[i] > max ) {
+      max = notlar[i];
+    }
+    if( notlar[i] < min ) {
+      min = notlar[i];
+    }
+  }
+  cout<<"ort:"<<toplam/n<<endl;
+  cout<<"max:"<<max<<endl;
+  cout<<"min:"<<min<<endl;
+*/
+//------------------------------------------
+/*
+	// 2D array
+	int** a = new int*[rowCount];
+	for(int i = 0; i < rowCount; ++i)
+    	a[i] = new int[colCount];
+	
+	// bellekten silinmesi
+	for(int i = 0; i < sizeY; ++i) {
+    	delete [] ary[i];
+	}
+	delete [] ary;
+*/  
+//------------------------------------------
+/*
+// öğrenci arasınav ortalamaları
+  int n,m;
+  cout<<"öğrenci sayısı:";  cin>>n;
+  cout<<"ödev sayısı:";     cin>>m;
+  double *sinifOrtalama   = new double[m]();
+  double *ogrOrtalama     = new double[n];
+  double **notlar         = new double*[n];
+  for(int i=0;i<n;i++){
+    notlar[i] = new double[m];
+  }
+  for(int i=0;i<n;i++){
+    cout<<i+1<<". öğrenci için:"<<endl;
+    for(int j=0;j<m;j++){
+      cout<<j+1<<". ödev notu:";
+      cin>>notlar[i][j];
+    }
+  }
+  for(int j=0;j<m;j++){
+    sinifOrtalama[j] = 0;
+  }
+  double satirtoplam = 0;
+  double sutuntoplam = 0;
+  double toplam = 0, ort = 0;
+  for(int i=0;i<n;i++){
+    sutuntoplam = 0;
+    for(int j=0;j<m;j++){
+      sutuntoplam       += notlar[i][j];
+      sinifOrtalama[j]  += notlar[i][j];
+    }
+    ogrOrtalama[i] = sutuntoplam / m;
+  }
+  for(int j=0;j<m;j++){
+    sinifOrtalama[j] /= n;
+  }
+  cout<<"Öğrenci Ortalamaları:"<<endl;
+  for(int i=0;i<n;i++){
+    cout<<i+1<<" >"<<ogrOrtalama[i]<<endl;
+  }
+  cout<<"Ödev Ortalamaları:"<<endl;
+  for(int j=0;j<m;j++){
+    cout<<j+1<<" >"<<sinifOrtalama[j]<<endl;
+  }
+  */
+//---------------------------------------
+
+// Yukarıdaki class yapısı kullanarak
+  Ders nyp;
+  nyp.notlarOku();
+  nyp.ogrOrtalamaBul();
+  nyp.odevOrtalamaBul();
+
+//---------------------------------------
+  return 0;
+}
